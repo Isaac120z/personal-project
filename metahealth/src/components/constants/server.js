10a5a7c491586010d282af9942599a02240f5dba
Isaac <Isaac120z@msn.com>
2018-05-04 15:17:30 -0500

    changed some stuff

--- metahealth/src/components/constants/server.js
+++ metahealth/src/components/constants/server.js
@@ -1,6 +1,6 @@
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "http://myapidomain.com"
-    : "http://localhost:3001";
+    ? "http://meta-health.fun"
+    : "http://www.meta-health.fun";
 
 export default PAYMENT_SERVER_URL;
